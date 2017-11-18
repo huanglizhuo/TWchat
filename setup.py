f595da5295b34903a6e77e2c6a4011cd1b29c8a2
huanglizhuo <huanglizhuo1991@gmail.com>
2017-11-18 22:11:09 +0800

    add travis config file

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='TWchat',
-    version='0.0.3',
+    version='0.0.4',
     packages=find_packages(),
     install_requires=[
         'itchat',
