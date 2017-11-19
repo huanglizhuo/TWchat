12c02e702ea5d2c2c919e1731043022c0fd2531c
huanglizhuo <huanglizhuo1991@gmail.com>
2017-11-19 22:34:21 +0800

    update to 0.0.6

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='TWchat',
-    version='0.0.5.2',
+    version='0.0.6',
     packages=find_packages(),
     install_requires=[
         'itchat',
