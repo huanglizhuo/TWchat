4ccfb478107fdaf570a27d55f50c402d2f04ff05
huanglizhuo <huanglizhuo1991@gmail.com>
2017-11-20 16:50:44 +0800

    add name sort

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='TWchat',
-    version='0.0.6.5',
+    version='0.0.7',
     packages=find_packages(),
     install_requires=[
         'itchat',
